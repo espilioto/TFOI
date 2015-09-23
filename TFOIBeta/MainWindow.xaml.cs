c840bceeb0618fcc529f98e736ddfe75c68ae4d4
espilioto <espilioto@gmail.com>
2015-09-23 23:01:04 +0300

    Moved main menu from the MainWindow to PageMain

--- TFOIBeta/MainWindow.xaml.cs
+++ TFOIBeta/MainWindow.xaml.cs
@@ -34,8 +34,7 @@ namespace TFOIBeta
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             mainWindow = this;
-
-            mainWindowFrame.Navigate(new menus.PageMain());
+            mainWindowFrame.NavigationService.Navigate(new menus.PageMain());
 
             Items.ParseJsonItemList();
             Bosses.ParseJsonBossList();
@@ -45,68 +44,8 @@ namespace TFOIBeta
             var player = new Player();
         }
 
-        #region main menu stuff
-        private void exit_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            this.Close();
-        }
-        private void exit_MouseEnter(object sender, MouseEventArgs e)
-        {
-            exit_.Visibility = Visibility.Visible;
-        }
-        private void exit_MouseLeave(object sender, MouseEventArgs e)
-        {
-            exit_.Visibility = Visibility.Hidden;
-        }
-        private void newrun_MouseEnter(object sender, MouseEventArgs e)
-        {
-            newrun_.Visibility = Visibility.Visible;
-        }
-        private void newrun_MouseLeave(object sender, MouseEventArgs e)
-        {
-            newrun_.Visibility = Visibility.Hidden;
-        }
-        private void runs_MouseEnter(object sender, MouseEventArgs e)
-        {
-            runs_.Visibility = Visibility.Visible;
-        }
-        private void runs_MouseLeave(object sender, MouseEventArgs e)
-        {
-            runs_.Visibility = Visibility.Hidden;
-        }
-        private void chars_MouseEnter(object sender, MouseEventArgs e)
-        {
-            chars_.Visibility = Visibility.Visible;
-        }
-        private void chars_MouseLeave(object sender, MouseEventArgs e)
-        {
-            chars_.Visibility = Visibility.Hidden;
-        }
-        private void items_MouseEnter(object sender, MouseEventArgs e)
-        {
-            items_.Visibility = Visibility.Visible;
-        }
-        private void items_MouseLeave(object sender, MouseEventArgs e)
-        {
-            items_.Visibility = Visibility.Hidden;
-        }
-        private void bosses_MouseEnter(object sender, MouseEventArgs e)
-        {
-            bosses_.Visibility = Visibility.Visible;
-        }
-        private void bosses_MouseLeave(object sender, MouseEventArgs e)
-        {
-            bosses_.Visibility = Visibility.Hidden;
-        }
-        private void options_MouseEnter(object sender, MouseEventArgs e)
-        {
-            options_.Visibility = Visibility.Visible;
-        }
-        private void options_MouseLeave(object sender, MouseEventArgs e)
-        {
-            options_.Visibility = Visibility.Hidden;
-        }
-        #endregion
+        
+
 
     }
 }
--- TFOIBeta/menus/PageMain.xaml.cs
+++ TFOIBeta/menus/PageMain.xaml.cs
@@ -24,9 +24,94 @@ namespace TFOIBeta.menus
     {
         public PageMain()
         {
-            InitializeComponent();
-
-            
+            InitializeComponent();   
         }
+
+        #region main menu stuff
+        private void exit_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mainWindow.Close();
+        }
+        private void exit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            exit_.Visibility = Visibility.Visible;
+        }
+        private void exit_MouseLeave(object sender, MouseEventArgs e)
+        {
+            exit_.Visibility = Visibility.Hidden;
+        }
+        private void newrun_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mainWindow.mainWindowFrame.NavigationService.Navigate(new menus.PageNewRun());
+        }
+        private void newrun_MouseEnter(object sender, MouseEventArgs e)
+        {
+            newrun_.Visibility = Visibility.Visible;
+        }
+        private void newrun_MouseLeave(object sender, MouseEventArgs e)
+        {
+            newrun_.Visibility = Visibility.Hidden;
+        }
+        private void runs_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mainWindow.mainWindowFrame.NavigationService.Navigate(new menus.PageRuns());
+        }
+        private void runs_MouseEnter(object sender, MouseEventArgs e)
+        {
+            runs_.Visibility = Visibility.Visible;
+        }
+        private void runs_MouseLeave(object sender, MouseEventArgs e)
+        {
+            runs_.Visibility = Visibility.Hidden;
+        }
+        private void chars_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mainWindow.mainWindowFrame.NavigationService.Navigate(new menus.PageChars());
+        }
+        private void chars_MouseEnter(object sender, MouseEventArgs e)
+        {
+            chars_.Visibility = Visibility.Visible;
+        }
+        private void chars_MouseLeave(object sender, MouseEventArgs e)
+        {
+            chars_.Visibility = Visibility.Hidden;
+        }
+        private void items_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mainWindow.mainWindowFrame.NavigationService.Navigate(new menus.PageItems());
+        }
+        private void items_MouseEnter(object sender, MouseEventArgs e)
+        {
+            items_.Visibility = Visibility.Visible;
+        }
+        private void items_MouseLeave(object sender, MouseEventArgs e)
+        {
+            items_.Visibility = Visibility.Hidden;
+        }
+        private void bosses_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mainWindow.mainWindowFrame.NavigationService.Navigate(new menus.PageBosses());
+        }
+        private void bosses_MouseEnter(object sender, MouseEventArgs e)
+        {
+            bosses_.Visibility = Visibility.Visible;
+        }
+        private void bosses_MouseLeave(object sender, MouseEventArgs e)
+        {
+            bosses_.Visibility = Visibility.Hidden;
+        }
+        private void options_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mainWindow.mainWindowFrame.NavigationService.Navigate(new menus.PageOptions());
+        }
+        private void options_MouseEnter(object sender, MouseEventArgs e)
+        {
+            options_.Visibility = Visibility.Visible;
+        }
+        private void options_MouseLeave(object sender, MouseEventArgs e)
+        {
+            options_.Visibility = Visibility.Hidden;
+        }
+        #endregion
     }
 }
