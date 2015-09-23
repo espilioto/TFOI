eea7ba713e9103bca1567cc59d6305813d75281a
espilioto <espilioto@gmail.com>
2015-09-23 17:11:28 +0300

    (Early version of the(?)) main menu is ready

--- TFOIBeta/Properties/Resources.Designer.cs
+++ TFOIBeta/Properties/Resources.Designer.cs
@@ -189,5 +189,15 @@ namespace TFOIBeta.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap underline {
+            get {
+                object obj = ResourceManager.GetObject("underline", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
--- TFOIBeta/obj/Debug/TFOIBeta_Content.g.i.cs
+++ TFOIBeta/obj/Debug/TFOIBeta_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/back.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/underline.png")]
 
 
