0c2b942d25ffb5c98270336a538632f0fdf22379
espilioto <espilioto@gmail.com>
2015-10-28 00:39:33 +0200

    Icon added to the executable :3

--- TFOIBeta/obj/Debug/TFOIBeta_Content.g.i.cs
+++ TFOIBeta/obj/Debug/TFOIBeta_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/underline.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon.ico")]
 
 
